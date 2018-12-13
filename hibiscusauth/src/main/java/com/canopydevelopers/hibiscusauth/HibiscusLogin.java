6eec0c2d8c2e957c8a2088e7659f19e4b2cd37b1
anmol2805 <saxenaanmol28@gmail.com>
2018-12-13 08:46:33 +0530

    hibiscusauth module completed

--- hibiscusauth/src/main/java/com/canopydevelopers/hibiscusauth/HibiscusLogin.java
+++ hibiscusauth/src/main/java/com/canopydevelopers/hibiscusauth/HibiscusLogin.java
@@ -1,14 +1,16 @@
 package com.canopydevelopers.hibiscusauth;
 
+import android.content.Context;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.canopydevelopers.mysingleton.Mysingleton;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class HibiscusLogin {
-    public static void logInWithUserCredentials(String student_id,String password){
+    public static void logInWithUserCredentials(String student_id, String password, Context context){
         JSONObject credentials = new JSONObject();
         try {
             credentials.put("uid",student_id);
@@ -16,18 +18,18 @@ public class HibiscusLogin {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "", credentials, new Response.Listener<JSONObject>() {
+        JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST, "http://14.139.198.171/api/hibi/login_test", credentials, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-
+                System.out.println("response:" + response);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                System.out.println("response:" + error);
             }
         });
-
+        Mysingleton.getInstance(context).addToRequestqueue(loginRequest);
 
     }
 }
