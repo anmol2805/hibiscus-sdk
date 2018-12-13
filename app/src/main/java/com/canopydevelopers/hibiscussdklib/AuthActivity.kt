8d57cf6439d2ec58a7358859eb0b170faceb096a
anmol2805 <saxenaanmol28@gmail.com>
2018-12-13 08:55:36 +0530

    testing time

--- app/src/main/java/com/canopydevelopers/hibiscussdklib/AuthActivity.kt
+++ app/src/main/java/com/canopydevelopers/hibiscussdklib/AuthActivity.kt
@@ -2,11 +2,13 @@ package com.canopydevelopers.hibiscussdklib
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.canopydevelopers.hibiscusauth.HibiscusLogin
 
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        HibiscusLogin.logInWithUserCredentials("b516008","anmol@2805",this)
     }
 }
